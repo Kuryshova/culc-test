af2213bc60dc6ad9550e02e8ac750b7a80996038
Kuryshova <dar_surprise@mail.ru>
2016-06-08 15:16:41 +0300

    2

---
+++ js/tempalate.js
@@ -0,0 +1,179 @@
+var array = [{title:'монитор', prise: 5000, quantity: 20},
+	{title:'мышь', prise: 500, quantity: 10},
+	{title:'системный блок', prise: 6000, quantity: 5},
+	{title:'принтер', prise: 5500, quantity: 10},
+	{title:'клавиатура', prise: 100, quantity: 30}];
+
+var selectArray = [];
+
+//var allEquipments = document.getElementById('all-equipments-id');
+
+
+function allEquipmentsShow(id, k) {
+
+//выделить в отдельную функцию создание самой таблицы
+
+ 	var tbody = document.getElementById(id).getElementsByTagName("tbody")[0];
+    var row = document.createElement("tr");
+
+    var td1 = document.createElement("td");
+    td1.appendChild(document.createTextNode(array[k].title));
+    var td2 = document.createElement("td");
+    td2.appendChild (document.createTextNode(array[k].prise));
+    var td3 = document.createElement("td");
+    td3.appendChild(document.createTextNode(array[k].quantity));
+    var td4 = document.createElement("td");
+    var button = createButtonAdd(td4, k);
+ 
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+    tbody.appendChild(row);
+}
+
+function createButtonAdd(context, l){
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "+";
+    button.className = "add add-"+l;
+    button.id = "idadd-"+ l;
+
+    context.appendChild(button);
+    button.setAttribute('onclick', 'javascript:EquipmentsAdd("all-equipments-id", "sel-equipments-id", ' + l + ')');
+
+}
+
+// по кнопкам так же бъединить в одну функцию
+
+function createButtonRem(context, l){
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "-";
+    button.className = "remowe remowe-"+l;
+    button.id = "idrem-"+ l;
+
+    context.appendChild(button);
+    button.setAttribute('onclick', 'javascript:EquipmentsRem("all-equipments-id", "sel-equipments-id", ' + l + ')');
+
+}
+
+function EquipmentsAdd(from ,id, k) {
+	if (array[k].quantity<=0) {
+
+		alert("Выбранного оборудования нет на скаде");
+	}
+	else{
+
+	array[k].quantity = array[k].quantity - 1;
+	var count = 1;
+
+
+
+//if (selectArray.length = 0) {
+
+	selectArray.push({title:array[k].title , prise: array[k].prise, quantity: count});
+
+/*} else {
+	for (var i = selectArray.length - 1; i >= 0; i--) {
+			if (selectArray[i].title==array[k].title) {
+				selectArray[i].quantity = selectArray[i].quantity + 1;
+				count = selectArray[i].quantity;
+			} else{
+			selectArray.push({title:array[k].title , prise: array[k].prise, quantity: count});	
+			}
+		}
+}
+
+
+if (selectArray.length > 0) {
+
+for (var i = selectArray.length; i > 0; i--) {
+	document.getElementById(id).getElementsByTagName("tbody")[0].deleteRow(i);
+}
+
+}*/
+
+
+
+
+ 	var tbody = document.getElementById(id).getElementsByTagName("tbody")[0];
+    var row = document.createElement("tr");
+
+
+    var td1 = document.createElement("td");
+    td1.appendChild(document.createTextNode(array[k].title));
+    var td3 = document.createElement("td");
+    td3.appendChild(document.createTextNode(count));
+    var td4 = document.createElement("td");
+    var button = createButtonRem(td4, k);
+ 
+    row.appendChild(td1);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+    tbody.appendChild(row);
+
+
+    for (var i = array.length-1; i >= 0; i--) {
+	document.getElementById(from).getElementsByTagName("tbody")[0].deleteRow(i+1);
+	}
+
+
+
+	for (var i = array.length - 1; i >= 0; i--) {
+	allEquipmentsShow(from, i);
+	}
+
+	}
+
+}
+
+function EquipmentsRem(from ,id, k) {
+
+	array[k].quantity = array[k].quantity + 1;
+
+	//document.getElementById(id).deleteRow(rowind);
+
+
+
+    for (var i = array.length-1; i >= 0; i--) {
+	document.getElementById(from).getElementsByTagName("tbody")[0].deleteRow(i+1);
+	}
+
+	for (var i = array.length - 1; i >= 0; i--) {
+	allEquipmentsShow(from, i);
+	}
+
+	
+
+}
+
+
+function calculation() {
+	var sum = 0;
+	for (var i = selectArray.length - 1; i >= 0; i--) {
+		sum = sum + (selectArray[i].prise * selectArray[i].quantity);
+	}
+	//alert(sum);
+	document.getElementById('result').innerHTML = "Ваша итоговая сумма: <span>"+sum+"</span>руб.";
+
+}
+
+
+window.onload = function(){
+
+for (var i = array.length - 1; i >= 0; i--) {
+	allEquipmentsShow('all-equipments-id', i);
+}
+
+}
+
+
+
+
+
+
+
+
